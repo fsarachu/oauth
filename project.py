53f927dfbe495d106fdd4aaa7a87277b3fb93a5b
fsarachu <fsarachu@gmail.com>
2016-12-26 11:44:55 -0300

    Add CRSF token generator

--- project.py
+++ project.py
@@ -7,6 +7,9 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+from flask import session as login_session
+import random, string
+
 # Connect to Database and create database session
 db_path = os.path.join(os.path.dirname(__file__), 'restaurantmenu.db')
 engine = create_engine('sqlite:///{}'.format(db_path))
@@ -14,6 +17,13 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+@app.route('/login')
+def show_login():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    return 'The current session state is {}'.format(login_session['state'])
 
 
 # JSON APIs to view Restaurant Information
