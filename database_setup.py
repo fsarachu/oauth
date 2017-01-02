d000d7350f6d19e50dd3a904bd145a1cb685712e
fsarachu <fsarachu@gmail.com>
2017-01-02 07:08:04 -0300

    Add User - Restaurant relationship

--- database_setup.py
+++ database_setup.py
@@ -21,6 +21,8 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
