30ac307c8f0d22d18f8d69573eeeccdc92ca409f
fsarachu <fsarachu@gmail.com>
2017-01-09 07:53:36 -0300

    Use done() method for ajax call, since "success()" is deprecated as of jQuery 3.0

--- static/googleLogin.js
+++ static/googleLogin.js
@@ -18,21 +18,26 @@ $(function () {
                 type: "POST",
                 url: "http://localhost:5000/auth/google?state=" + state,
                 contentType: "application/octet-stream; charset=utf-8",
-                success: function (result) {
-                    if (result) {
-                        $("#result").html("Login Successful!<br>" + result + "<br> Redirecting...");
-                        setTimeout(function () {
-                            window.location.replace("http://localhost:5000/");
-                        }, 1000);
-                    } else if (authResult["error"]) {
-                        console.log("Ooooooops! Something went wrong: " + authResult["error"]);
-                    } else {
-                        $("#result").html("Failed to make a server-side call.")
-                    }
-                },
                 processData: false,
                 data: authResult['code']
-            });
+            })
+                .done(function (result) {
+                        var $result = $("#result");
+
+                        $result.removeClass("hidden");
+
+                        if (result) {
+                            $result.addClass("alert-success").text("Login Successful! Redirecting...");
+                            setTimeout(function () {
+                                window.location.replace("http://localhost:5000/");
+                            }, 3000);
+                        } else if (authResult["error"]) {
+                            $result.addClass("alert-danger").text("Ooooooops! Something went wrong: " + authResult["error"]);
+                        } else {
+                            $result.addClass("alert-danger").text("Failed to make a server-side call.");
+                        }
+                    }
+                );
         } else {
             // There was an error.
             alert("Ooooops! no code here");
