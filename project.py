ba9f04bf9b2b9add0781e13e11e07a83fe2db4bf
fsarachu <fsarachu@gmail.com>
2017-01-11 07:06:20 -0300

    Define get_user_info()

--- project.py
+++ project.py
@@ -43,6 +43,11 @@ def create_user(login_session):
     return user.id
 
 
+def get_user_info(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
 @app.route('/login')
 def show_login():
     return render_template("login.html", state=csrf_token())
